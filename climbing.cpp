e3c86fefa5294d040dd32de59569090c7b1e2513
habiba2312 <habibarezk2312@gmail.com>
2025-09-09 20:23:11 +0300

    Update climbing.cpp

---
+++ climbing.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int climbStairs(int n) {
+    if (n <= 2) return n;
+    int a = 1, b = 2;
+    for (int i = 3; i <= n; i++) {
+        int temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return b;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << climbStairs(n);
+}
